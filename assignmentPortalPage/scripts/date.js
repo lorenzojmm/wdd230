7425650340666174a2df3f3e74b8675da60ab628
lorenzojmm <lorenzo.jensen2@gmail.com>
2022-09-26 18:02:20 -0600

    created WDD 230 Assignment Portal

---
+++ assignmentPortalPage/scripts/date.js
@@ -0,0 +1,64 @@
+/* let daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = year[d.getFullYear()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+document.getElementById("date2").textContent = fulldate; */
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.querySelector("#date1").textContent = year;
+
+let text = document.lastModified;
+document.getElementById("date2").innerHTML = text;
